3ed5f3419112ff03517cc76e8fb1b958cbcb2ad9
Mitchell Long and Snow Vilay <mitchell@>
2017-09-05 16:22:08 -0700

    will add functions to replace letters with numbers and search for an s in the first postion to not be replaced

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,14 +1,24 @@
-
 class String
   def leetspeak
     array = Array.new()
     letters = self.split("")
+    counter =0
     letters.each do |letter|
+
       if (letter === "e")
         array.push("3")
+      elsif (letter === "o")
+        array.push("0")
+      elsif (letter === "I")
+        array.push("1")
+      elsif (counter === 0 && letter === "s")
+        array.push(letter)
+      elsif (letter === "s")
+          array.push("z")
       else
         array.push(letter)
       end
+      counter = counter + 1
     end
     array.join("")
   end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -4,12 +4,24 @@ require('pry')
 
 describe('#leetspeak') do
   it('returns a string as is when no leetspeak rules apply') do
-    expect(("elephant").leetspeak).to(eq("elephant"))
+    expect(("happy").leetspeak).to(eq("happy"))
   end
   it('returns an array of letters from the inputted word') do
-    expect(("elephant").leetspeak).to(eq(["e","l","e","p","h","a","n","t"]))
+    expect(("lphant").leetspeak).to(eq("lphant"))
   end
   it('replace e with "3"') do
     expect(("elephant").leetspeak).to(eq("3l3phant"))
+  end
+  it('replaces every "o" in a string with a "0"') do
+    expect(("boo boo").leetspeak).to(eq("b00 b00"))
+  end
+  it('replaces every "I" in a string with a "1"') do
+   expect(("I like Ike").leetspeak).to(eq("1 lik3 1k3"))
+ end
+ it('replaces every "s" in a string with a "z"') do
+   expect(("roses").leetspeak).to(eq("r0z3z"))
+ end
+ it('does NOT replace the first letter when it is an s') do
+    expect(("sassafrass").leetspeak).to(eq("sazzafrazz"))
   end
 end
